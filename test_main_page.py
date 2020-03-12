8398d343d335ec4b8405d3af54af4de6a953d4a3
Anastasia <wugevece@gmail.com>
2020-03-12 21:35:18 +0300

    init LoginPage in test

--- test_main_page.py
+++ test_main_page.py
@@ -7,8 +7,8 @@ def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, link)
     page.open() 
     page.go_to_login_page()
-    loginpage = LoginPage(browser, link)
-    loginpage.should_be_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
     
 def test_guest_should_see_login_link(browser):
