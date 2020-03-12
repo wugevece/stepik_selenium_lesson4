47f3868920e186e6469f5bb24b32e560a384887b
Anastasia <wugevece@gmail.com>
2020-03-12 17:59:30 +0300

    add should_be_login_page

--- conftest.py
+++ conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='en',
                      help="Choose language")
